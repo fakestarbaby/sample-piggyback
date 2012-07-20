91767f08d90b7119fd2d37f0f3faebf2acfd72c6
fakestarbaby <fakestarbaby@gmail.com>
2012-07-20 10:24:56 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplePiggyback::Application.config.secret_token = '3aeeb1a24f9e51f08382d2a99d1ff67511d42911c91c6db2ff7b15f0817b3ceea591ed8a1158515724268afea24e4843dd2931437714e374f0f18556d2011dc2'
